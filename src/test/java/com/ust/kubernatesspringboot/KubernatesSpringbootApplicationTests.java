b72a2271658efb2c39df67dac8f0c5e502f0e553
Mighamaria <mighka.maria2@gamil.com>
2023-06-13 10:49:45 +0530

    kubeantes

---
+++ src/test/java/com/ust/kubernatesspringboot/KubernatesSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ust.kubernatesspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KubernatesSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
